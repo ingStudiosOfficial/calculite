245f8c8b6bcde72b76a3044734b9c24b1c04dfd4
Ethan Lee <pluscompanyhqDEPT@gmail.com>
2025-12-31 17:39:21 +0800

    Add android bubblewrap

---
+++ android/app/src/main/java/io/github/ingstudiosofficial/calculite/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.ingstudiosofficial.calculite;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
