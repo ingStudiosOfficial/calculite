ce89ad642a6c482771793ec078e67fbb76bde322
Ethan Lee <pluscompanyhqDEPT@gmail.com>
2026-01-12 18:56:35 +0800

    commit before restructuring

---
+++ extension/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import webExtension from 'vite-plugin-web-extension';
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    webExtension({
+      manifest: './manifest.json',
+      browser: 'chrome',
+    }),
+  ],
+});
